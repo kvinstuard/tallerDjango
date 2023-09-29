1bb4ea471e8c04261bdbd18a326f9d0961a7070d
Invitado <invitado@s1pc3.eisc.univalle.edu.co>
2023-09-29 08:24:21 -0500

    Creación Modelos

--- BackendDjango/models.py
+++ BackendDjango/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class ArticuloDeportivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    deporte = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    valor = models.CharField(max_length = 100)
+
+class Univallunos(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos= models.CharField(max_length=100)
+    esEstudiante = models.BooleanField(default=False)
+    tipoDocumento = models.CharField(max_length=100)
+    numeroDocumento = models.CharField(max_length=100)
+    codigoEstudiante = models.CharField(max_length=100)
+    correo = models.CharField(max_length=100)
+
+
+
