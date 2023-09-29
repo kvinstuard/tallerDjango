a78dfef343d18baed4333dd18a001125a0cdcfaf
Invitado <invitado@s1pc2.eisc.univalle.edu.co>
2023-09-29 08:59:16 -0500

    menú de administrador

--- BackendDjango/admin.py
+++ BackendDjango/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Univallunos, ArticuloDeportivo
 
 # Register your models here.
+admin.site.register(Univallunos)
+admin.site.register(ArticuloDeportivo)
