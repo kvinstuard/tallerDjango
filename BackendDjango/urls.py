cef82e460fdcf56f819464c54371eadd303942cb
Juan Diaz Villota <juandiazvillota@Juans-MacBook-Air.local>
2023-10-01 08:52:25 -0500

    trabajando en las vistas html

--- BackendDjango/urls.py
+++ BackendDjango/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.bienvenida),
     path('univallunos/', views.univallunos),
-    path('articulos/', views.articulos_deportivos)
+    path('articulos/', views.articulos_deportivos),
+    path('prestamos/', views.prestamos),
+    path('multas/', views.multas),
+    path('reportes/', views.reportes)
 ]
--- BackendDjango/views.py
+++ BackendDjango/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+miscrip = """<div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>"""
+
 # Create your views here.
 
 def univallunos(request):
-   return HttpResponse("<h1>seccion univallunos en desarrollo</h1>")
+   return HttpResponse("<script src='univallunos.js'></script>")
 
 def articulos_deportivos(request):
-   return HttpResponse("<h1>seccion articulos deportivos en desarrollo</h1>")
+   return HttpResponse("<script src='articulos_deportivos.js'></script>")
+
+def prestamos(request):
+   return HttpResponse("<script src='prestamos.js'></script>")
+
+def multas(request):
+   return HttpResponse("<script src='multas.js'></script>")
+
+def reportes(request):
+   return HttpResponse("<script src='reportes.js'></script>")
 
 def bienvenida(request):
-   return HttpResponse("<h1>seccion bienvenida en desarrollo</h1>")
+   return render(request, 'index.html')
